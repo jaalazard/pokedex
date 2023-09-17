e7826106979dbc6527a31559476cdb13de890c75
jaalazard <ja.alazard@proton.me>
2023-09-17 13:47:33 +0200

    commit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,29 @@
 function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="un pokemon"
-      />
+      {pokemon.imgSrc ? (
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="un pokemon"
+        />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name.toUpperCase()}</figcaption>
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
