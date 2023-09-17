99faa5936f7e8a2990883f2de9a24fde16943530
jaalazard <ja.alazard@proton.me>
2023-09-17 15:45:56 +0200

    propTypes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
   console.log(props);
   return (
@@ -13,6 +15,13 @@ function PokemonCard(props) {
       <figcaption>{props.pokemon.name.toUpperCase()}</figcaption>
     </figure>
   );
+}
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
 }
 
 export default PokemonCard;
